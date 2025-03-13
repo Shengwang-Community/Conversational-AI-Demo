63e882ba2fdd07e8eaeedaca7d2a07bda692841a
czhen <56986964+shczhen@users.noreply.github.com>
2025-03-13 05:37:26 +0000

    fix: update anis usage

--- Web/Scenes/VoiceAgent/src/services/rtc.ts
+++ Web/Scenes/VoiceAgent/src/services/rtc.ts
@@ -121,7 +121,7 @@ export class RtcService extends EventService<IRtcEvents> {
     }
     logger.log(CONSOLE_PREFIX, '[DenoiserProcessor]', 'Initializing')
     const denoiser = new AIDenoiserExtension({
-      assetsPath: '/denoiser/v2/external',
+      assetsPath: '/denoiser/external',
     })
     // Check compatibility
     if (!denoiser.checkCompatibility()) {
@@ -235,9 +235,6 @@ export class RtcService extends EventService<IRtcEvents> {
           !!this.processor
         )
         audioTrack.pipe(this.processor).pipe(audioTrack.processorDestination)
-        // close ains.agc
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        ;(this.processor as any).setParameter({ agcConfig: { enabled: false } })
       }
       this.localTracks.audioTrack = audioTrack
       // must be called after pipe processor
