c4f2355be428e6cdc957d217ce060433c45317f7
zhangwei <zhangwei03@shengwang.cn>
2025-06-30 16:20:33 +0800

    [Android]pts Remove 20ms.

--- Android/scenes/convoai/src/main/java/io/agora/scene/convoai/convoaiApi/subRender/v3/TranscriptionController.kt
+++ Android/scenes/convoai/src/main/java/io/agora/scene/convoai/convoaiApi/subRender/v3/TranscriptionController.kt
@@ -251,7 +251,7 @@ internal class TranscriptionController(private val config: TranscriptionConfig) 
                 presentationMs: Long
             ): Boolean {
                 // Pass render time to transcription controller
-                mPresentationMs = presentationMs + 20
+                mPresentationMs = presentationMs
                 return false
             }
 
