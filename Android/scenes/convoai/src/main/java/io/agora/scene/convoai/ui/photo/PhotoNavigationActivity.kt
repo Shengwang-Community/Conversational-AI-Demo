48f7092dae3f1ac621980d013e1b9819feef967a
HeZhengQing <hezhengqing@agora.io>
2025-07-15 17:16:53 +0800

    [FIX] take photo preview scale

--- Android/scenes/convoai/src/main/java/io/agora/scene/convoai/ui/photo/PhotoNavigationActivity.kt
+++ Android/scenes/convoai/src/main/java/io/agora/scene/convoai/ui/photo/PhotoNavigationActivity.kt
@@ -73,12 +73,6 @@ class PhotoNavigationActivity : BaseActivity<CovPhotoNavigationActivityBinding>(
         }
         
         supportFragmentManager.beginTransaction()
-            .setCustomAnimations(
-                R.anim.slide_in_right,
-                0,
-                0,
-                R.anim.slide_out_right
-            )
             .replace(R.id.fragment_container, fragment, "take_photo")
             .addToBackStack("take_photo")
             .commit()
@@ -123,12 +117,6 @@ class PhotoNavigationActivity : BaseActivity<CovPhotoNavigationActivityBinding>(
         
         runOnUiThread {
             supportFragmentManager.beginTransaction()
-                .setCustomAnimations(
-                    R.anim.slide_in_right,
-                    0,
-                    0,
-                    R.anim.slide_out_right
-                )
                 .replace(R.id.fragment_container, fragment, "photo_edit")
                 .addToBackStack("photo_edit") 
                 .commitAllowingStateLoss()
