2f41d99dd1f5563b2365b5faca0f21d0f25cd0a5
HeZhengQing <hezhengqing@agora.io>
2025-07-17 17:48:04 +0800

    [FIX] ios login

--- iOS/Scenes/ConvoAI/ConvoAI/ConvoAI/Classes/Main/Chat/ChatViewController+BottomBarControl.swift
+++ iOS/Scenes/ConvoAI/ConvoAI/ConvoAI/Classes/Main/Chat/ChatViewController+BottomBarControl.swift
@@ -59,42 +59,6 @@ extension ChatViewController {
         stopAgent()
     }
     
-    private func clickTheStartButton() async {
-        addLog("[Call] clickTheStartButton()")
-        let loginState = UserCenter.shared.isLogin()
-
-        if loginState {
-            await MainActor.run {
-                let needsShowMicrophonePermissionAlert = PermissionManager.getMicrophonePermission() == .denied
-                if needsShowMicrophonePermissionAlert {
-                    self.bottomBar.setMircophoneButtonSelectState(state: true)
-                }
-            }
-            
-            PermissionManager.checkMicrophonePermission { res in
-                Task {
-                    await self.prepareToStartAgent()
-                    await MainActor.run {
-                        if !res {
-                            self.bottomBar.setMircophoneButtonSelectState(state: true)
-                        }
-                    }
-                }
-            }
-            
-            return
-        }
-        
-        await MainActor.run {
-            let loginVC = LoginViewController()
-            loginVC.modalPresentationStyle = .overFullScreen
-            loginVC.loginAction = { [weak self] in
-                self?.goToSSOViewController()
-            }
-            self.present(loginVC, animated: false)
-        }
-    }
-    
     private func clickCaptionsButton(state: Bool) {
         showTranscription(state: !state)
     }
@@ -119,7 +83,7 @@ extension ChatViewController {
     }
     
     @MainActor
-    private func prepareToStartAgent() async {
+    func prepareToStartAgent() async {
         startLoading()
     
         Task {
@@ -157,34 +121,6 @@ extension ChatViewController {
         addLog("setupMuteState: \(state)")
         agentStateView.setMute(state)
         rtcManager.muteLocalAudio(mute: state)
-    }
-    
-    private func goToSSOViewController() {
-        let ssoWebVC = SSOWebViewController()
-        let baseUrl = AppContext.shared.baseServerUrl
-        ssoWebVC.urlString = "\(baseUrl)/v1/convoai/sso/login"
-        ssoWebVC.completionHandler = { [weak self] token in
-            guard let self = self else { return }
-            if let token = token {
-                self.addLog("SSO token: \(token)")
-                let model = LoginModel()
-                model.token = token
-                AppContext.loginManager()?.updateUserInfo(userInfo: model)
-                let localToken = UserCenter.user?.token ?? ""
-                self.addLog("local token: \(localToken)")
-                self.bottomBar.startLoadingAnimation()
-                LoginApiService.getUserInfo { [weak self] error in
-                    self?.bottomBar.stopLoadingAnimation()
-                    if let err = error {
-                        AppContext.loginManager()?.logout()
-                        SVProgressHUD.showInfo(withStatus: err.localizedDescription)
-                    }
-                }
-            } else {
-                AppContext.loginManager()?.logout()
-            }
-        }
-        self.navigationController?.pushViewController(ssoWebVC, animated: false)
     }
     
     internal func startLoading() {
